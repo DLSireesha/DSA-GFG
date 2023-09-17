40465b5f12e83a8035e3217dc4f7648decb94bb9
Sireesha18 <116700405+DLSireesha@users.noreply.github.com>
2023-09-17 10:45:45 +0530

    Updated Solution - GfG to GitHub

--- Basic/Print first n Fibonacci Numbers/print-first-n-fibonacci-numbers.cpp
+++ Basic/Print first n Fibonacci Numbers/print-first-n-fibonacci-numbers.cpp
@@ -15,11 +15,12 @@ class Solution
     vector<long long> printFibb(int n) 
     {
         //code here
-        vector<long long> dp(n,-1);
-        dp[0]=1;
-        dp[1]=1;
+        vector<long long> dp;
+        dp.push_back(1);
+        if(n==1) return dp;
+        dp.push_back(1);
         for(int i=2;i<n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            dp.push_back(dp[i-1]+dp[i-2]);
         }
         return dp;
     }
