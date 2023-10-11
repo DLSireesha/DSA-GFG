da680bbb51e734515b120521cc3a9f133d852828
Sireesha18 <116700405+DLSireesha@users.noreply.github.com>
2023-10-11 17:48:38 +0530

    Updated Solution - GfG to GitHub

--- Easy/Check for Balanced Tree/check-for-balanced-tree.cpp
+++ Easy/Check for Balanced Tree/check-for-balanced-tree.cpp
@@ -102,24 +102,24 @@ struct Node
  */
 
 class Solution{
-    private:
-    int height(struct Node* node){
-        if(node==NULL) return 0;
-        int left=height(node->left);
-        int right=height(node->right);
+    public:
+    //Function to check whether a binary tree is balanced or not.
+    int height(Node* root){
+        if(root==NULL) return 0;
+        int left=height(root->left);
+        int right=height(root->right);
         int ans=max(left,right)+1;
         return ans;
     }
-    public:
-    //Function to check whether a binary tree is balanced or not.
     bool isBalanced(Node *root)
     {
         if(root==NULL) return true;
-        bool left=isBalanced(root->left);
-        bool right=isBalanced(root->right);
-        bool dif=abs(height(root->left)-height(root->right))<=1;
-        if(left && right && dif) return 1;
-        else return false;
+        int l=height(root->left);
+        int r=height(root->right);
+        if(abs(l-r)<=1 && isBalanced(root->left) && isBalanced(root->right)){
+            return true;
+        }
+        return false;
     }
 };
 
