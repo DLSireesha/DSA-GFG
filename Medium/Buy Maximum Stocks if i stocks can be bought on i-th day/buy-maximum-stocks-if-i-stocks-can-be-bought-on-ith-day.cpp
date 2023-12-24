52531e12c02e8ea8f2e25b8503563aca94252d5d
Sireesha18 <116700405+DLSireesha@users.noreply.github.com>
2023-12-24 19:18:49 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Buy Maximum Stocks if i stocks can be bought on i-th day/buy-maximum-stocks-if-i-stocks-can-be-bought-on-ith-day.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int buyMaximumProducts(int n, int k, int price[]){
+        //Write your code here
+        multimap<int,int> m;
+        for(int i=0;i<n;i++){
+            m.insert({price[i],i+1});
+        }
+        int money=k,ans=0;
+        for(auto it:m){
+            money-=it.second*it.first;
+            if(money<0){
+                money+=it.first*it.second;
+                ans+=money/it.first;
+                break;
+            }
+            ans+=it.second;
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n, k;
+	    cin >> n >> k;
+	    int price[n];
+	    for(int i = 0 ; i < n; i++){
+	        cin >> price[i];
+	    }
+	    Solution ob;
+	    int ans = ob.buyMaximumProducts(n, k, price);
+	    cout << ans<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
